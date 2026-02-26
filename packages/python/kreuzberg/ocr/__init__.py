chikingsley/kreuzberg

feat(ocr): add real rapid-ocr backend integration

packages/python/kreuzberg/ocr/__init__.py
@@ -5,6 +5,7 @@ auto-registered when kreuzberg is imported (if their dependencies are installed)
 
 Each backend has a separate optional dependency group:
 - EasyOCR: pip install "kreuzberg[easyocr]"
+- RapidOCR: pip install "kreuzberg[rapidocr]"
 
 Note: PaddleOCR is now a native Rust backend available in all non-WASM bindings
 via the 'paddle-ocr' feature flag. No Python dependency is required.
@@ -12,7 +13,7 @@ via the 'paddle-ocr' feature flag. No Python dependency is required.
 
 from __future__ import annotations
 
-__all__ = ["EasyOCRBackend", "OcrBackendProtocol"]
+__all__ = ["EasyOCRBackend", "RapidOCRBackend", "OcrBackendProtocol"]
 
 from kreuzberg.ocr.protocol import OcrBackendProtocol
 
@@ -20,3 +21,8 @@ try:
     from kreuzberg.ocr.easyocr import EasyOCRBackend
 except ImportError:
     EasyOCRBackend = None  # type: ignore[assignment,misc]
+
+try:
+    from kreuzberg.ocr.rapidocr import RapidOCRBackend
+except ImportError:
+    RapidOCRBackend = None  # type: ignore[assignment,misc]

xberg-io/xberg

packages/python/kreuzberg/ocr/__init__.py
@@ -5,6 +5,7 @@ auto-registered when kreuzberg is imported (if their dependencies are installed)
 
 Each backend has a separate optional dependency group:
 - EasyOCR: pip install "kreuzberg[easyocr]"
+- RapidOCR: pip install "kreuzberg[rapidocr]"
 
 Note: PaddleOCR is now a native Rust backend available in all non-WASM bindings
 via the 'paddle-ocr' feature flag. No Python dependency is required.
@@ -12,7 +13,7 @@ via the 'paddle-ocr' feature flag. No Python dependency is required.
 
 from __future__ import annotations
 
-__all__ = ["EasyOCRBackend", "OcrBackendProtocol"]
+__all__ = ["EasyOCRBackend", "OcrBackendProtocol", "RapidOCRBackend"]
 
 from kreuzberg.ocr.protocol import OcrBackendProtocol
 
@@ -20,3 +21,8 @@ try:
     from kreuzberg.ocr.easyocr import EasyOCRBackend
 except ImportError:
     EasyOCRBackend = None  # type: ignore[assignment,misc]
+
+try:
+    from kreuzberg.ocr.rapidocr import RapidOCRBackend
+except ImportError:
+    RapidOCRBackend = None  # type: ignore[assignment,misc]