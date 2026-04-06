geyerandreas/kreuzberg

feat(e2e-generator): update core logic and parity for all language generators

tools/e2e-generator/src/python.rs
@@ -1,5 +1,6 @@
 use crate::fixtures::{
-    Assertions, ExtractionMethod, Fixture, InputType, PluginAssertions, PluginTestSpec, RenderAssertions,
+    Assertions, EmbedAssertions, EmbedSpec, ExtractionMethod, Fixture, InputType, PluginAssertions,
+    PluginTestSpec, RenderAssertions,
 };
 use anyhow::{Context, Result};
 use camino::Utf8Path;
@@ -691,6 +692,39 @@ def assert_is_png(data: bytes) -> None:
 def assert_min_byte_length(data: bytes, min_length: int) -> None:
     """Assert data is at least min_length bytes."""
     assert len(data) >= min_length, f"Expected at least {min_length} bytes, got {len(data)}"
+
+
+def assert_embed_result(
+    result: list[list[float]],
+    *,
+    count: int | None = None,
+    dimensions: int | None = None,
+    no_nan: bool = False,
+    no_inf: bool = False,
+    non_zero: bool = False,
+    normalized: bool = False,
+) -> None:
+    """Assert properties of a standalone embed() result."""
+    import math
+
+    assert isinstance(result, list), f"Expected list of embeddings, got {type(result)}"
+    if count is not None:
+        assert len(result) == count, f"Expected {count} embeddings, got {len(result)}"
+    for i, vec in enumerate(result):
+        assert isinstance(vec, list), f"Embedding {i} is not a list"
+        if dimensions is not None:
+            assert len(vec) == dimensions, f"Embedding {i}: expected {dimensions} dims, got {len(vec)}"
+        if no_nan:
+            nan_vals = [v for v in vec if math.isnan(v)]
+            assert not nan_vals, f"Embedding {i} contains NaN values"
+        if no_inf:
+            inf_vals = [v for v in vec if math.isinf(v)]
+            assert not inf_vals, f"Embedding {i} contains Inf values"
+        if non_zero:
+            assert any(v != 0.0 for v in vec), f"Embedding {i} is all zeros"
+        if normalized:
+            norm = math.sqrt(sum(v * v for v in vec))
+            assert abs(norm - 1.0) < 1e-4, f"Embedding {i} L2 norm {norm:.6f} != 1.0 (not normalized)"
 "#;
 
 pub fn generate(fixtures: &[Fixture], output_root: &Utf8Path) -> Result<()> {
@@ -731,6 +765,14 @@ pub fn generate(fixtures: &[Fixture], output_root: &Utf8Path) -> Result<()> {
         sorted.sort_by(|a, b| a.id.cmp(&b.id));
         let content = render_render_category(&sorted)?;
         fs::write(tests_dir.join("test_render.py"), content).context("Failed to write Python render test file")?;
+    }
+
+    let embed_fixtures: Vec<_> = fixtures.iter().filter(|f| f.is_embed()).collect();
+    if !embed_fixtures.is_empty() {
+        let mut sorted = embed_fixtures;
+        sorted.sort_by(|a, b| a.id.cmp(&b.id));
+        let content = generate_embed_tests_python(&sorted)?;
+        fs::write(tests_dir.join("test_embed.py"), content).context("Failed to write Python embed test file")?;
     }
 
     Ok(())
@@ -1448,6 +1490,136 @@ fn render_render_assertions_python(assertions: &RenderAssertions, var: &str, cod
         writeln!(code, "    helpers.assert_min_byte_length({var}, {min_len})")?;
     }
     Ok(())
+}
+
+fn generate_embed_tests_python(fixtures: &[&Fixture]) -> Result<String> {
+    let mut buf = String::new();
+    writeln!(buf, "# Code generated by kreuzberg-e2e-generator. DO NOT EDIT.")?;
+    writeln!(buf, "# To regenerate: cargo run -p kreuzberg-e2e-generator -- generate --lang python")?;
+    writeln!(buf, "#")?;
+    writeln!(buf, "# Standalone embed() / embed_sync() tests.")?;
+    writeln!(buf, "from __future__ import annotations\n")?;
+    writeln!(buf, "import pytest")?;
+    writeln!(buf, "from kreuzberg import EmbeddingConfig, EmbeddingModelType, embed_sync")?;
+    writeln!(buf)?;
+    writeln!(buf, "from . import helpers\n")?;
+    buf.push('\n');
+
+    for fixture in fixtures {
+        buf.push_str(&render_embed_test(fixture)?);
+        buf.push('\n');
+    }
+
+    Ok(buf)
+}
+
+fn render_embed_test(fixture: &Fixture) -> Result<String> {
+    let mut code = String::new();
+    let spec = fixture.embed_spec();
+    let assertions = fixture.embed_assertions();
+    let is_async = spec.async_variant;
+    let test_name = format!("test_{}", sanitize_identifier(&fixture.id));
+
+    // Skip guards
+    let requires_feature = &fixture.skip().requires_feature;
+    let skip_platforms = &fixture.skip().skip_on_platform;
+
+    if is_async {
+        writeln!(code, "@pytest.mark.asyncio")?;
+        writeln!(code, "async def {test_name}() -> None:")?;
+    } else {
+        writeln!(code, "def {test_name}() -> None:")?;
+    }
+    writeln!(code, "    \"\"\"{}\"\"\"", escape_python_string(&fixture.description))?;
+    writeln!(code)?;
+
+    // Platform skip
+    if !skip_platforms.is_empty() {
+        let conditions: Vec<String> = skip_platforms
+            .iter()
+            .filter_map(|triple| rust_triple_to_python_condition(triple))
+            .collect();
+        if !conditions.is_empty() {
+            let combined = conditions.join(" or ");
+            writeln!(code, "    import platform as _platform")?;
+            writeln!(code, "    if {combined}:")?;
+            writeln!(code, "        pytest.skip(\"Skipping {}: not supported on this platform\")", fixture.id)?;
+            writeln!(code)?;
+        }
+    }
+
+    // Build embedding config
+    let model_name = spec
+        .config
+        .get("model")
+        .and_then(|m| m.get("name"))
+        .and_then(|n| n.as_str())
+        .unwrap_or("balanced");
+    let normalize = spec
+        .config
+        .get("normalize")
+        .and_then(|v| v.as_bool())
+        .unwrap_or(false);
+    let batch_size = spec.config.get("batch_size").and_then(|v| v.as_u64());
+
+    writeln!(code, "    _model = EmbeddingModelType.preset({})", python_string_literal(model_name))?;
+    if let Some(bs) = batch_size {
+        writeln!(
+            code,
+            "    _config = EmbeddingConfig(model=_model, normalize={}, batch_size={})",
+            if normalize { "True" } else { "False" },
+            bs
+        )?;
+    } else {
+        writeln!(
+            code,
+            "    _config = EmbeddingConfig(model=_model, normalize={})",
+            if normalize { "True" } else { "False" }
+        )?;
+    }
+    writeln!(code)?;
+
+    // Input texts
+    let texts_literal = render_python_string_list(&spec.texts);
+    let await_prefix = if is_async { "await " } else { "" };
+    let func = if is_async { "embed" } else { "embed_sync" };
+
+    if is_async {
+        writeln!(code, "    from kreuzberg import embed as _embed")?;
+        writeln!(code, "    result = {await_prefix}_embed({texts_literal}, config=_config)")?;
+    } else {
+        writeln!(code, "    result = {func}({texts_literal}, config=_config)")?;
+    }
+    writeln!(code)?;
+
+    // Assertions
+    code.push_str(&render_embed_assertions(&assertions));
+
+    Ok(code)
+}
+
+fn render_embed_assertions(a: &EmbedAssertions) -> String {
+    let mut buf = String::new();
+    let count_lit = a.count.map(|c| c.to_string()).unwrap_or_else(|| "None".to_string());
+    let dims_lit = a.dimensions.map(|d| d.to_string()).unwrap_or_else(|| "None".to_string());
+    writeln!(
+        buf,
+        "    helpers.assert_embed_result("
+    ).unwrap();
+    writeln!(buf, "        result,").unwrap();
+    writeln!(buf, "        count={count_lit},").unwrap();
+    writeln!(buf, "        dimensions={dims_lit},").unwrap();
+    writeln!(buf, "        no_nan={},", if a.no_nan { "True" } else { "False" }).unwrap();
+    writeln!(buf, "        no_inf={},", if a.no_inf { "True" } else { "False" }).unwrap();
+    writeln!(buf, "        non_zero={},", if a.non_zero { "True" } else { "False" }).unwrap();
+    writeln!(buf, "        normalized={},", if a.normalized { "True" } else { "False" }).unwrap();
+    writeln!(buf, "    )").unwrap();
+    buf
+}
+
+fn render_python_string_list(items: &[String]) -> String {
+    let parts: Vec<String> = items.iter().map(|s| python_string_literal(s)).collect();
+    format!("[{}]", parts.join(", "))
 }
 
 fn generate_plugin_api_tests(fixtures: &[&Fixture], output_dir: &Utf8Path) -> Result<()> {

tools/e2e-generator/src/rust.rs
@@ -1,4 +1,7 @@
-use crate::fixtures::{Assertions, ExtractionMethod, Fixture, InputType, PluginTestSpec, RenderAssertions};
+use crate::fixtures::{
+    Assertions, EmbedAssertions, EmbedSpec, ExtractionMethod, Fixture, InputType, PluginTestSpec,
+    RenderAssertions,
+};
 use anyhow::{Context, Result};
 use camino::Utf8Path;
 use itertools::Itertools;
@@ -43,6 +46,14 @@ pub fn generate(fixtures: &[Fixture], output_root: &Utf8Path) -> Result<()> {
         render_fixtures.sort_by(|a, b| a.id.cmp(&b.id));
         let content = render_render_tests(&render_fixtures)?;
         let path = tests_dir.join("render_test.rs");
+        fs::write(&path, content).with_context(|| format!("Writing {}", path))?;
+    }
+
+    let mut embed_fixtures: Vec<_> = fixtures.iter().filter(|f| f.is_embed()).collect();
+    if !embed_fixtures.is_empty() {
+        embed_fixtures.sort_by(|a, b| a.id.cmp(&b.id));
+        let content = generate_embed_tests_rust(&embed_fixtures)?;
+        let path = tests_dir.join("embed_test.rs");
         fs::write(&path, content).with_context(|| format!("Writing {}", path))?;
     }
 
@@ -1172,4 +1183,101 @@ fn generate_object_property_assertions_rust(
     }
 
     Ok(())
+}
+
+fn generate_embed_tests_rust(fixtures: &[&Fixture]) -> Result<String> {
+    let mut buf = String::new();
+    writeln!(buf, "// Code generated by kreuzberg-e2e-generator. DO NOT EDIT.")?;
+    writeln!(buf, "// To regenerate: cargo run -p kreuzberg-e2e-generator -- generate --lang rust")?;
+    writeln!(buf, "\n// Tests for standalone embed() fixtures.")?;
+    writeln!(buf, "#![allow(clippy::too_many_lines)]")?;
+    writeln!(buf, "use e2e_rust::assertions;")?;
+    writeln!(buf, "use kreuzberg::core::config::{{EmbeddingConfig, EmbeddingModelType}};")?;
+    writeln!(buf, "use std::sync::Arc;")?;
+    writeln!(buf)?;
+
+    for fixture in fixtures {
+        buf.push_str(&render_embed_test(fixture)?);
+    }
+
+    Ok(buf)
+}
+
+fn render_embed_test(fixture: &Fixture) -> Result<String> {
+    let mut code = String::new();
+    let spec = fixture.embed_spec();
+    let assertions = fixture.embed_assertions();
+    let is_async = spec.async_variant;
+    let test_name = format!("test_{}", sanitize_identifier(&fixture.id));
+
+    if is_async {
+        writeln!(code, "#[tokio::test]")?;
+        writeln!(code, "async fn {test_name}() {{")?;
+    } else {
+        writeln!(code, "#[test]")?;
+        writeln!(code, "fn {test_name}() {{")?;
+    }
+    writeln!(code, "    // {}", escape_rust_string(&fixture.description))?;
+
+    if !fixture.skip().requires_feature.is_empty() || !fixture.document().requires_external_tool.is_empty() {
+        writeln!(
+            code,
+            "    #[cfg(not(feature = \"embeddings\"))]\n    return;"
+        )?;
+    }
+
+    let skip_platforms = &fixture.skip().skip_on_platform;
+    if !skip_platforms.is_empty() {
+        let conditions = skip_platforms.iter().filter_map(|p| match p.as_str() {
+            "x86_64-pc-windows-msvc" => Some("all(target_os = \"windows\", target_env = \"msvc\")"),
+            "aarch64-apple-darwin" => Some("all(target_arch = \"aarch64\", target_os = \"macos\")"),
+            _ => None,
+        }).collect::<Vec<_>>();
+        if !conditions.is_empty() {
+            let combined = conditions.join(", ");
+            writeln!(code, "    #[cfg(any({}))]", combined)?;
+            writeln!(code, "    return;")?;
+        }
+    }
+
+    let model_name = spec.config.get("model").and_then(|m| m.get("name")).and_then(|n| n.as_str()).unwrap_or("balanced");
+    let normalize = spec.config.get("normalize").and_then(|v| v.as_bool()).unwrap_or(false);
+    let batch_size = spec.config.get("batch_size").and_then(|v| v.as_u64());
+
+    writeln!(code, "    let model = EmbeddingModelType::Preset(\"{model_name}\".to_string());")?;
+    if let Some(bs) = batch_size {
+        writeln!(code, "    let config = EmbeddingConfig {{\n        model,\n        normalize: {normalize},\n        batch_size: {bs},\n    }};")?;
+    } else {
+        writeln!(code, "    let config = EmbeddingConfig {{\n        model,\n        normalize: {normalize},\n        ..Default::default()\n    }};")?;
+    }
+    writeln!(code)?;
+
+    let texts_literal = spec.texts.iter().map(|s| format!("\"{}\"", escape_rust_string(s))).collect::<Vec<_>>().join(", ");
+    writeln!(code, "    let texts = vec![{texts_literal}];")?;
+
+    if is_async {
+        writeln!(code, "    let result_res = kreuzberg::embeddings::embed_texts_async(&texts, Arc::new(config)).await;")?;
+    } else {
+        writeln!(code, "    let result_res = kreuzberg::embeddings::embed_texts(&texts, &config);")?;
+    }
+
+    if spec.config.get("model").and_then(|m| m.get("name")).is_some_and(|n| n.as_str() == Some("invalid")) {
+        writeln!(code, "    assert!(result_res.is_err());\n}}")?;
+        return Ok(code);
+    } else {
+        writeln!(code, "    let result = result_res.expect(\"Embedding failed\");")?;
+    }
+
+    let count_lit = assertions.count.map(|c| format!("Some({c})")).unwrap_or_else(|| "None".into());
+    let dim_lit = assertions.dimensions.map(|d| format!("Some({d})")).unwrap_or_else(|| "None".into());
+    writeln!(
+        code,
+        "    assertions::assert_embed_result(&result, {count_lit}, {dim_lit}, {no_nan}, {no_inf}, {non_zero}, {normalized});\n}}\n",
+        no_nan = assertions.no_nan,
+        no_inf = assertions.no_inf,
+        non_zero = assertions.non_zero,
+        normalized = assertions.normalized,
+    )?;
+
+    Ok(code)
 }

xberg-io/xberg

tools/e2e-generator/src/python.rs
@@ -1,6 +1,6 @@
 use crate::fixtures::{
-    Assertions, ExtractionMethod, Fixture, GenerationMode, InputType, PluginAssertions, PluginTestSpec,
-    RenderAssertions,
+    Assertions, EmbedAssertions, EmbedSpec, ExtractionMethod, Fixture, GenerationMode, InputType,
+    PluginAssertions, PluginTestSpec, RenderAssertions,
 };
 use crate::parity::{self, ParityManifest, TypeDef};
 use anyhow::{Context, Result};
@@ -726,6 +726,39 @@ def assert_is_png(data: bytes) -> None:
 def assert_min_byte_length(data: bytes, min_length: int) -> None:
     """Assert data is at least min_length bytes."""
     assert len(data) >= min_length, f"Expected at least {min_length} bytes, got {len(data)}"
+
+
+def assert_embed_result(
+    result: list[list[float]],
+    *,
+    count: int | None = None,
+    dimensions: int | None = None,
+    no_nan: bool = False,
+    no_inf: bool = False,
+    non_zero: bool = False,
+    normalized: bool = False,
+) -> None:
+    """Assert properties of a standalone embed() result."""
+    import math
+
+    assert isinstance(result, list), f"Expected list of embeddings, got {type(result)}"
+    if count is not None:
+        assert len(result) == count, f"Expected {count} embeddings, got {len(result)}"
+    for i, vec in enumerate(result):
+        assert isinstance(vec, list), f"Embedding {i} is not a list"
+        if dimensions is not None:
+            assert len(vec) == dimensions, f"Embedding {i}: expected {dimensions} dims, got {len(vec)}"
+        if no_nan:
+            nan_vals = [v for v in vec if math.isnan(v)]
+            assert not nan_vals, f"Embedding {i} contains NaN values"
+        if no_inf:
+            inf_vals = [v for v in vec if math.isinf(v)]
+            assert not inf_vals, f"Embedding {i} contains Inf values"
+        if non_zero:
+            assert any(v != 0.0 for v in vec), f"Embedding {i} is all zeros"
+        if normalized:
+            norm = math.sqrt(sum(v * v for v in vec))
+            assert abs(norm - 1.0) < 1e-4, f"Embedding {i} L2 norm {norm:.6f} != 1.0 (not normalized)"
 "#;
 
 pub fn generate(fixtures: &[Fixture], output_root: &Utf8Path, mode: &GenerationMode) -> Result<()> {
@@ -767,6 +800,14 @@ pub fn generate(fixtures: &[Fixture], output_root: &Utf8Path, mode: &GenerationM
         sorted.sort_by(|a, b| a.id.cmp(&b.id));
         let content = render_render_category(&sorted)?;
         fs::write(tests_dir.join("test_render.py"), content).context("Failed to write Python render test file")?;
+    }
+
+    let embed_fixtures: Vec<_> = fixtures.iter().filter(|f| f.is_embed()).collect();
+    if !embed_fixtures.is_empty() {
+        let mut sorted = embed_fixtures;
+        sorted.sort_by(|a, b| a.id.cmp(&b.id));
+        let content = generate_embed_tests_python(&sorted)?;
+        fs::write(tests_dir.join("test_embed.py"), content).context("Failed to write Python embed test file")?;
     }
 
     Ok(())
@@ -1599,6 +1640,136 @@ fn render_render_assertions_python(assertions: &RenderAssertions, var: &str, cod
         writeln!(code, "    helpers.assert_min_byte_length({var}, {min_len})")?;
     }
     Ok(())
+}
+
+fn generate_embed_tests_python(fixtures: &[&Fixture]) -> Result<String> {
+    let mut buf = String::new();
+    writeln!(buf, "# Code generated by kreuzberg-e2e-generator. DO NOT EDIT.")?;
+    writeln!(buf, "# To regenerate: cargo run -p kreuzberg-e2e-generator -- generate --lang python")?;
+    writeln!(buf, "#")?;
+    writeln!(buf, "# Standalone embed() / embed_sync() tests.")?;
+    writeln!(buf, "from __future__ import annotations\n")?;
+    writeln!(buf, "import pytest")?;
+    writeln!(buf, "from kreuzberg import EmbeddingConfig, EmbeddingModelType, embed_sync")?;
+    writeln!(buf)?;
+    writeln!(buf, "from . import helpers\n")?;
+    buf.push('\n');
+
+    for fixture in fixtures {
+        buf.push_str(&render_embed_test(fixture)?);
+        buf.push('\n');
+    }
+
+    Ok(buf)
+}
+
+fn render_embed_test(fixture: &Fixture) -> Result<String> {
+    let mut code = String::new();
+    let spec = fixture.embed_spec();
+    let assertions = fixture.embed_assertions();
+    let is_async = spec.async_variant;
+    let test_name = format!("test_{}", sanitize_identifier(&fixture.id));
+
+    // Skip guards
+    let requires_feature = &fixture.skip().requires_feature;
+    let skip_platforms = &fixture.skip().skip_on_platform;
+
+    if is_async {
+        writeln!(code, "@pytest.mark.asyncio")?;
+        writeln!(code, "async def {test_name}() -> None:")?;
+    } else {
+        writeln!(code, "def {test_name}() -> None:")?;
+    }
+    writeln!(code, "    \"\"\"{}\"\"\"", escape_python_string(&fixture.description))?;
+    writeln!(code)?;
+
+    // Platform skip
+    if !skip_platforms.is_empty() {
+        let conditions: Vec<String> = skip_platforms
+            .iter()
+            .filter_map(|triple| rust_triple_to_python_condition(triple))
+            .collect();
+        if !conditions.is_empty() {
+            let combined = conditions.join(" or ");
+            writeln!(code, "    import platform as _platform")?;
+            writeln!(code, "    if {combined}:")?;
+            writeln!(code, "        pytest.skip(\"Skipping {}: not supported on this platform\")", fixture.id)?;
+            writeln!(code)?;
+        }
+    }
+
+    // Build embedding config
+    let model_name = spec
+        .config
+        .get("model")
+        .and_then(|m| m.get("name"))
+        .and_then(|n| n.as_str())
+        .unwrap_or("balanced");
+    let normalize = spec
+        .config
+        .get("normalize")
+        .and_then(|v| v.as_bool())
+        .unwrap_or(false);
+    let batch_size = spec.config.get("batch_size").and_then(|v| v.as_u64());
+
+    writeln!(code, "    _model = EmbeddingModelType.preset({})", python_string_literal(model_name))?;
+    if let Some(bs) = batch_size {
+        writeln!(
+            code,
+            "    _config = EmbeddingConfig(model=_model, normalize={}, batch_size={})",
+            if normalize { "True" } else { "False" },
+            bs
+        )?;
+    } else {
+        writeln!(
+            code,
+            "    _config = EmbeddingConfig(model=_model, normalize={})",
+            if normalize { "True" } else { "False" }
+        )?;
+    }
+    writeln!(code)?;
+
+    // Input texts
+    let texts_literal = render_python_string_list(&spec.texts);
+    let await_prefix = if is_async { "await " } else { "" };
+    let func = if is_async { "embed" } else { "embed_sync" };
+
+    if is_async {
+        writeln!(code, "    from kreuzberg import embed as _embed")?;
+        writeln!(code, "    result = {await_prefix}_embed({texts_literal}, config=_config)")?;
+    } else {
+        writeln!(code, "    result = {func}({texts_literal}, config=_config)")?;
+    }
+    writeln!(code)?;
+
+    // Assertions
+    code.push_str(&render_embed_assertions(&assertions));
+
+    Ok(code)
+}
+
+fn render_embed_assertions(a: &EmbedAssertions) -> String {
+    let mut buf = String::new();
+    let count_lit = a.count.map(|c| c.to_string()).unwrap_or_else(|| "None".to_string());
+    let dims_lit = a.dimensions.map(|d| d.to_string()).unwrap_or_else(|| "None".to_string());
+    writeln!(
+        buf,
+        "    helpers.assert_embed_result("
+    ).unwrap();
+    writeln!(buf, "        result,").unwrap();
+    writeln!(buf, "        count={count_lit},").unwrap();
+    writeln!(buf, "        dimensions={dims_lit},").unwrap();
+    writeln!(buf, "        no_nan={},", if a.no_nan { "True" } else { "False" }).unwrap();
+    writeln!(buf, "        no_inf={},", if a.no_inf { "True" } else { "False" }).unwrap();
+    writeln!(buf, "        non_zero={},", if a.non_zero { "True" } else { "False" }).unwrap();
+    writeln!(buf, "        normalized={},", if a.normalized { "True" } else { "False" }).unwrap();
+    writeln!(buf, "    )").unwrap();
+    buf
+}
+
+fn render_python_string_list(items: &[String]) -> String {
+    let parts: Vec<String> = items.iter().map(|s| python_string_literal(s)).collect();
+    format!("[{}]", parts.join(", "))
 }
 
 fn generate_plugin_api_tests(fixtures: &[&Fixture], output_dir: &Utf8Path) -> Result<()> {

tools/e2e-generator/src/rust.rs
@@ -1,5 +1,6 @@
 use crate::fixtures::{
-    Assertions, ExtractionMethod, Fixture, GenerationMode, InputType, PluginTestSpec, RenderAssertions,
+    Assertions, EmbedAssertions, EmbedSpec, ExtractionMethod, Fixture, GenerationMode, InputType,
+    PluginTestSpec, RenderAssertions,
 };
 use crate::parity::{self, ParityManifest, TypeDef};
 use anyhow::{Context, Result};
@@ -71,6 +72,14 @@ pub fn generate(fixtures: &[Fixture], output_root: &Utf8Path, mode: &GenerationM
     // Generate tests/e2e.rs as the single entry point
     let e2e_content = "// Code generated by kreuzberg-e2e-generator. DO NOT EDIT.\n\nmod fixtures;\n";
     fs::write(tests_dir.join("e2e.rs"), e2e_content).context("Failed to write tests/e2e.rs")?;
+
+    let mut embed_fixtures: Vec<_> = fixtures.iter().filter(|f| f.is_embed()).collect();
+    if !embed_fixtures.is_empty() {
+        embed_fixtures.sort_by(|a, b| a.id.cmp(&b.id));
+        let content = generate_embed_tests_rust(&embed_fixtures)?;
+        let path = tests_dir.join("embed_test.rs");
+        fs::write(&path, content).with_context(|| format!("Writing {}", path))?;
+    }
 
     Ok(())
 }
@@ -1415,4 +1424,101 @@ fn rust_type_path(type_name: &str) -> String {
         "Keyword" => "kreuzberg::keywords::Keyword".to_string(),
         _ => format!("kreuzberg::{type_name}"),
     }
+}
+
+fn generate_embed_tests_rust(fixtures: &[&Fixture]) -> Result<String> {
+    let mut buf = String::new();
+    writeln!(buf, "// Code generated by kreuzberg-e2e-generator. DO NOT EDIT.")?;
+    writeln!(buf, "// To regenerate: cargo run -p kreuzberg-e2e-generator -- generate --lang rust")?;
+    writeln!(buf, "\n// Tests for standalone embed() fixtures.")?;
+    writeln!(buf, "#![allow(clippy::too_many_lines)]")?;
+    writeln!(buf, "use e2e_rust::assertions;")?;
+    writeln!(buf, "use kreuzberg::core::config::{{EmbeddingConfig, EmbeddingModelType}};")?;
+    writeln!(buf, "use std::sync::Arc;")?;
+    writeln!(buf)?;
+
+    for fixture in fixtures {
+        buf.push_str(&render_embed_test(fixture)?);
+    }
+
+    Ok(buf)
+}
+
+fn render_embed_test(fixture: &Fixture) -> Result<String> {
+    let mut code = String::new();
+    let spec = fixture.embed_spec();
+    let assertions = fixture.embed_assertions();
+    let is_async = spec.async_variant;
+    let test_name = format!("test_{}", sanitize_identifier(&fixture.id));
+
+    if is_async {
+        writeln!(code, "#[tokio::test]")?;
+        writeln!(code, "async fn {test_name}() {{")?;
+    } else {
+        writeln!(code, "#[test]")?;
+        writeln!(code, "fn {test_name}() {{")?;
+    }
+    writeln!(code, "    // {}", escape_rust_string(&fixture.description))?;
+
+    if !fixture.skip().requires_feature.is_empty() || !fixture.document().requires_external_tool.is_empty() {
+        writeln!(
+            code,
+            "    #[cfg(not(feature = \"embeddings\"))]\n    return;"
+        )?;
+    }
+
+    let skip_platforms = &fixture.skip().skip_on_platform;
+    if !skip_platforms.is_empty() {
+        let conditions = skip_platforms.iter().filter_map(|p| match p.as_str() {
+            "x86_64-pc-windows-msvc" => Some("all(target_os = \"windows\", target_env = \"msvc\")"),
+            "aarch64-apple-darwin" => Some("all(target_arch = \"aarch64\", target_os = \"macos\")"),
+            _ => None,
+        }).collect::<Vec<_>>();
+        if !conditions.is_empty() {
+            let combined = conditions.join(", ");
+            writeln!(code, "    #[cfg(any({}))]", combined)?;
+            writeln!(code, "    return;")?;
+        }
+    }
+
+    let model_name = spec.config.get("model").and_then(|m| m.get("name")).and_then(|n| n.as_str()).unwrap_or("balanced");
+    let normalize = spec.config.get("normalize").and_then(|v| v.as_bool()).unwrap_or(false);
+    let batch_size = spec.config.get("batch_size").and_then(|v| v.as_u64());
+
+    writeln!(code, "    let model = EmbeddingModelType::Preset(\"{model_name}\".to_string());")?;
+    if let Some(bs) = batch_size {
+        writeln!(code, "    let config = EmbeddingConfig {{\n        model,\n        normalize: {normalize},\n        batch_size: {bs},\n    }};")?;
+    } else {
+        writeln!(code, "    let config = EmbeddingConfig {{\n        model,\n        normalize: {normalize},\n        ..Default::default()\n    }};")?;
+    }
+    writeln!(code)?;
+
+    let texts_literal = spec.texts.iter().map(|s| format!("\"{}\"", escape_rust_string(s))).collect::<Vec<_>>().join(", ");
+    writeln!(code, "    let texts = vec![{texts_literal}];")?;
+
+    if is_async {
+        writeln!(code, "    let result_res = kreuzberg::embeddings::embed_texts_async(&texts, Arc::new(config)).await;")?;
+    } else {
+        writeln!(code, "    let result_res = kreuzberg::embeddings::embed_texts(&texts, &config);")?;
+    }
+
+    if spec.config.get("model").and_then(|m| m.get("name")).is_some_and(|n| n.as_str() == Some("invalid")) {
+        writeln!(code, "    assert!(result_res.is_err());\n}}")?;
+        return Ok(code);
+    } else {
+        writeln!(code, "    let result = result_res.expect(\"Embedding failed\");")?;
+    }
+
+    let count_lit = assertions.count.map(|c| format!("Some({c})")).unwrap_or_else(|| "None".into());
+    let dim_lit = assertions.dimensions.map(|d| format!("Some({d})")).unwrap_or_else(|| "None".into());
+    writeln!(
+        code,
+        "    assertions::assert_embed_result(&result, {count_lit}, {dim_lit}, {no_nan}, {no_inf}, {non_zero}, {normalized});\n}}\n",
+        no_nan = assertions.no_nan,
+        no_inf = assertions.no_inf,
+        non_zero = assertions.non_zero,
+        normalized = assertions.normalized,
+    )?;
+
+    Ok(code)
 }