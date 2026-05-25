42641a2496c7edc84a0acad1df3e7ed1230bee98
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-05-25 02:13:34 +0200

    chore: regenerate bindings with alef 0.19.5 (elixir+ruby+php AHashMap fix, swift snapshot accept)

--- packages/elixir/mix.exs
+++ packages/elixir/mix.exs
@@ -19,7 +19,7 @@ defmodule Kreuzberg.MixProject do
       licenses: ["Elastic-2.0"],
       links: %{"GitHub" => "https://github.com/kreuzberg-dev/kreuzberg"},
       files:
-        ~w(lib .formatter.exs mix.exs README* checksum-*.exs native/kreuzberg_nif/Cargo.toml native/kreuzberg_nif/Cargo.lock native/kreuzberg_nif/src)
+        ~w(lib .formatter.exs mix.exs README* checksum-*.exs native/kreuzberg_nif/Cargo.toml native/kreuzberg_nif/Cargo.lock ../../packages/elixir/native/kreuzberg_nif/src)
     ]
   end
 
